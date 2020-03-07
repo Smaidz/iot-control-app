598ecc6dd8cd0dd54f92870ceb77865082c43a23
Smaidz <eduards.spila@gmx.com>
2020-03-08 00:08:56 +0200

    fix

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static('.'));
-
 app.get('/', function(req,res) {
 	res.sendFile(path.join(__dirname,'index.html'));
 });
