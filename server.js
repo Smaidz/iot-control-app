e3f1d2a5d4968e843748d0fcff85bc1aa0802014
Smaidz <eduards.spila@gmx.com>
2020-03-07 23:53:06 +0200

    fix

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 app.use(express.static('.'));
 
 app.get('/', function(req,res) {
-	res.send("Test");
+	res.send("index.html");
 });
 
 app.listen(process.env.PORT || 8080);
