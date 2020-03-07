de0b3ef3ea1c93e520213506d14c35dff13c2640
Smaidz <eduards.spila@gmx.com>
2020-03-07 23:47:31 +0200

    fix attempt

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'iot-control-app';
+  title = 'iot-control-interface-app';
 }
