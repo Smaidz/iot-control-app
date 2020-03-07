1490afd897956383a43979ae2960ab44cccb5b78
Smaidz <eduards.spila@gmx.com>
2020-03-07 23:05:04 +0200

    fix

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static('./dist/iot-control-interface-app'));
+app.use(express.static('./dist/src/iot-control-interface-app'));
 
 app.get('/*', function(req,res) {
-	res.sendFile(path.join(__dirname,'/dist/iot-control-interface-app/index.html'));
+	res.sendFile(path.join(__dirname,'/dist/iot-control-interface-app/src/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
